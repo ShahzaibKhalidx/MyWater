072bf2bc2a47de582158cfa7f959735eff77efbd
ShahzaibKhalidx <shahzaib1638@gmail.com>
2024-07-21 22:14:04 +0500

    fixed survey modal (empty)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7f6522b446e0bd7"; }
+/******/ 		__webpack_require__.h = function() { return "d0fbda9c630b39e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/sections/SurveyModal.jsx
+++ src/components/sections/SurveyModal.jsx
@@ -1,18 +1,20 @@
+import { Model } from "survey-core";
 import { Survey } from 'survey-react-ui';
-import 'survey-core/modern.min.css';
+import 'survey-core/defaultV2.css'
 import { Modal } from 'react-bootstrap';
-import { useState } from 'react';
-import {surveyJson} from '@data/sections/surveyConfig';
+import { surveyJson } from '@data/sections/surveyConfig';
+// import emailjs from '@emailjs/browser'
 
 
 const SurveyModal = ({ show, handleClose }) => {
+  const model = new Model(surveyJson);
   return (
     <Modal show={show} onHide={handleClose} centered size="lg">
       <Modal.Header closeButton>
         {/* <Modal.Title>Survey</Modal.Title> */}
       </Modal.Header>
       <Modal.Body>
-        <Survey json={surveyJson} />
+        <Survey model={model} />
       </Modal.Body>
     </Modal>
   );
